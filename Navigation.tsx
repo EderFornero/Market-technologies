4c9718c67ad11c7c537c593ff1ea3b624f07f4cd
EderFornero <ederfornero.ef@gmail.com>
2024-01-11 21:09:16 -0300

    vector navigation

--- Navigation.tsx
+++ Navigation.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { createMaterialBottomTabNavigator, MaterialBottomTabNavigationOptions } from '@react-navigation/material-bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-//screens
+// screens
 import MainTechScreen from './src/screens/MainTechScreen'
 import DesktopAppTechScreen from './src/screens/DesktopAppTechScreen'
 import MobileAppTechScreen from './src/screens/MobileAppTechScreen'
@@ -10,9 +11,34 @@ import DatabasesScreen from './src/screens/DatabasesScreen'
 
 const Tab = createMaterialBottomTabNavigator()
 
-function MyTabs() { 
+type TabBarIconProps = {
+  focused: boolean;
+  color: string;
+  size: number;
+}
+
+function MyTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      initialRouteName='MainTech'
+      screenOptions={({ route }): MaterialBottomTabNavigationOptions => ({
+        tabBarIcon: ({ focused, color}) => {
+          let iconName: string = '';
+
+          if (route.name === 'MainTech') {
+            iconName = 'home';
+          } else if (route.name === 'MobileTech') {
+            iconName = 'tablet-android';
+          } else if (route.name === 'DesktopTech') {
+            iconName = 'desktop-tower-monitor';
+          } else if (route.name === 'Databases') {
+            iconName = 'database';
+          }
+
+          return <MaterialCommunityIcons name={iconName as any} size={25}  color={color} />;
+        },
+      })}
+    >
       <Tab.Screen name='MainTech' component={MainTechScreen} />
       <Tab.Screen name='MobileTech' component={MobileAppTechScreen} />
       <Tab.Screen name='DesktopTech' component={DesktopAppTechScreen} />
@@ -21,9 +47,8 @@ function MyTabs() {
   )
 }
 
-
-const Navigation = () => { 
-  return <NavigationContainer><MyTabs/></NavigationContainer>
+const Navigation = () => {
+  return <NavigationContainer><MyTabs /></NavigationContainer>
 }
 
 export default Navigation
